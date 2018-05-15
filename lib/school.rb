bc773f9ba00d8d74a571f44a45a6b87ac7b9a13d
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-05-15 16:09:40 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_accessor :school, :roster
+
+  def initialize(school)
+    # initializes a school with a blank roster
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    #adds student to roster as a value for the key of the grade they are in
+    @name = name
+    @grade = grade
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    #should take in a grade and list all students in that grade
+    roster[grade]
+  end
+
+  def sort
+    sorted_roster = {}
+    roster.each do |grade, students|
+      sorted_roster[grade] = students.sort
+    end
+    sorted_roster
+  end
+
+end
